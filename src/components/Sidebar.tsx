3841b602577879169ae92896f9a535d305ed007b
khameleonstorm <khameleonstorm@gmail.com>
2024-09-29 02:07:44 +0100

    logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -67,7 +67,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <NavLink to="/">
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" className='h-9 w-auto'/>
         </NavLink>
 
         <button
